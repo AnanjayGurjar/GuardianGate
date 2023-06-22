7431bf0c8867fcf40503633596991b00bfdc789b
AnanjayGurjar <ananjaygurjar@outlook.com>
2023-06-22 09:32:26 +0530

    caching added

--- routes/weatherRoute.js
+++ routes/weatherRoute.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const { test, getWeather } = require("../controllers/weatherController");
+const apicache = require("apicache");
+
+//initialize the cache
+let cache = apicache.middleware;
 
 router.route("/test").get(test);
-router.route("/get").get(getWeather);
+router.route("/get").get(cache("2 minutes"), getWeather);
 
 module.exports = router;
